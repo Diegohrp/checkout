7f2ecba401ac553561f71fbc999bc332f9a96dbb
diegohp <diego.blake11@gmail.com>
2022-06-25 23:32:33 -0500

    [FIX] config

---
+++ src/routes/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import {Home} from '../containers/Home';
+import {Checkout} from '../containers/Checkout';
+import {Information} from '../containers/Information';
+import {Payment} from '../containers/Payment';
+import {Success} from '../containers/Success';
+import {NotFound} from '../containers/NotFound';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/checkout" element={<Checkout />}></Route>
+        <Route path="/checkout/information" element={<Information />}></Route>
+        <Route path="/checkout/payment" element={<Payment />}></Route>
+        <Route path="/checkout/success" element={<Success />}></Route>
+        <Route path="*" element={<NotFound />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export {App};
---
+++ webpack.config.js
@@ -0,0 +1,70 @@
+/** @type {import('webpack').Configuration} */
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js',
+    publicPath: './',
+    clean: true,
+  },
+  mode: 'production',
+  resolve: {
+    extensions: ['.js', '.jsx'],
+    alias: {
+      /* '@components': path.resolve(__dirname, 'src/components'),
+      '@containers': path.resolve(__dirname, 'src/containers'),
+      '@pages': path.resolve(__dirname, 'src/pages'),
+      '@styles': path.resolve(__dirname, 'src/styles'),
+      '@icons': path.resolve(__dirname, 'src/assets/icons'),
+      '@images': path.resolve(__dirname, 'src/assets/img'), */
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+          },
+        ],
+      },
+      {
+        test: /\.s?css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        type: 'asset',
+        generator: {
+          filename: 'public/[hash][ext]',
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      filename: './index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+    }),
+  ],
+  optimization: {
+    minimize: true,
+    minimizer: [new CssMinimizerPlugin(), new TerserPlugin()],
+  },
+};
