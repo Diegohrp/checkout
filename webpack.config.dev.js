f493a96a9041c8b7e8f7ea701c45d72b14814a3d
diegohp <diego.blake11@gmail.com>
2022-06-30 22:22:43 -0500

    [UPDATE] webpack config

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -3,6 +3,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const Dotenv = require('dotenv-webpack');
+const webpack = require('webpack');
 module.exports = {
   entry: './src/index.js',
   output: {
@@ -64,7 +65,22 @@ module.exports = {
     new MiniCssExtractPlugin({
       filename: '[name].css',
     }),
-    new Dotenv(),
+    new Dotenv({
+      path: './.env',
+      safe: true,
+      systemvars: true,
+      defaults: false,
+    }),
+    new webpack.DefinePlugin({
+      'process.env': {
+        PAYPAL_CLIENT_ID: JSON.stringify(
+          process.env.PAYPAL_CLIENT_ID
+        ),
+        GOOGLE_MAPS_API_KEY: JSON.stringify(
+          process.env.GOOGLE_MAPS_API_KEY
+        ),
+      },
+    }),
   ],
   devServer: {
     static: path.join(__dirname, 'build'),
